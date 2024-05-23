068dc6ac1714ae37e297c91e6ceeec6e8943ffe8
Cecile <113383436+cecileherbermann@users.noreply.github.com>
2024-05-23 13:06:37 +0200

    Add files via upload

--- slurmjob.sh
+++ slurmjob.sh
@@ -2,23 +2,23 @@
 #SBATCH --job-name=finetune_scGPT  
 #SBATCH --output=finetune_scGPT.out
 #SBATCH --error=finetune_scGPT.err
-#SBATCH --time=12:00:00
+#SBATCH --time=6:00:00
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
 #SBATCH --mem=80G
-#SBATCH --partition=short
+#SBATCH --partition=gpu-medium
+#SBATCH --gpus-per-node=1
 
 # Load the conda environment
 conda activate scGPT
 
 # Change directory to scGPT directory
-cd "C:/Users/jzuyd/Documents/LACDR/PhD/scRNAseq data/scGPT/"
+cd "/projects/0/prjs1045"
 
 # Paths
-model_path = "scGPT_human"
-raw_data_path = "CFS/CFS_all_days_rawcount.h5ad"
-processed_data_path = "CFS/CFS_all_days_processed.h5ad"
-day = "CFS_Day0"
+model_path = "/scGPT_human"
+raw_data_path = "/CFS_data/CFS_all_days_rawcount.h5ad"
+processed_data_path = "CFS_data/CFS_all_days_processed.h5ad"
+day = "CFS_Day28"
 
 # Run the script
 python finetuning_slurmjob.py --model_path $model_path --raw_data_path $raw_data_path --processed_data_path $processed_data_path --day $day
